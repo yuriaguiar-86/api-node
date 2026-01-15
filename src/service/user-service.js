d394a9af765d81a6fd1474c2e2cf60cd700263c8
Yuri Aguiar <yuri_aguiar@11rm.eb.mil.br>
2026-01-15 08:23:47 -0300

    (feat) aplicado a transicao para controle de dados via teste

--- src/service/user-service.js
+++ src/service/user-service.js
@@ -5,26 +5,30 @@ class UserService {
         return await User.findAll();
     }
 
-    async findById(id) {
-        const user = await User.findByPk(id);
+    async findById(id, transaction) {
+        const user = await User.findByPk(id, { transaction });
         if (!user) throw new Error('Usuário não encontrado');
         return user;
     }
 
-    async create(userData) {
-        const user = await User.create(userData);
-        return user;
+    async create(email, password, transaction) {
+        return await User.create({
+            email, password
+        }, { transaction });
     }
 
-    async update(id, userData) {
-        const user =await this.findById(id);
-        await user.update(userData);
-        return user;
+    async update(id, email, password, transaction) {
+        const user = await this.findById(id, transaction);
+
+        user.email = email ?? user.email;
+        user.password = password ?? user.password;
+
+        return await user.save({ transaction });
     }
 
-    async destroy(id) {
-        const user = await this.findById(id);
-        await user.destroy();
+    async destroy(id, transaction) {
+        const user = await this.findById(id, transaction);
+        await user.destroy({ transaction });
         return true;
     }
 }
