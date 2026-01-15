68e44af544000bffc44fde9a468f3f8868dc8cf8
Yuri Aguiar <yuri_aguiar@11rm.eb.mil.br>
2026-01-15 08:21:42 -0300

    (feat) testes iniciais para crud de usuarios

--- test/user.test.js
+++ test/user.test.js
@@ -1,29 +1,49 @@
 const connection = require('../src/model/connection');
+const userService = require('../src/service/user-service');
 
 describe('Test users', () => {
     let transaction;
     let userId;
 
     beforeAll(async () => {
-        this.transaction = await connection.sequelize.transaction();
+        transaction = await connection.sequelize.transaction();
     });
 
     afterAll(async () => {
-        await this.transaction.rollback();
+        await transaction.rollback();
     });
 
     // Add
-    it('create a new user', () => {
+    it('create a new user', async () => {
+        const user = {
+            email: 'teste@email.com',
+            password: 'Senha@123'
+        };
 
+        const response = await userService.create(user.email, user.password, transaction);
+        
+        userId = response.user.id;
+
+        expect(response.user.email).toBe(user.email);
+        expect(response.user.password).toBe(user.password);
     });
 
     // Edit
-    it('update a user', () => {
+    it('update a user', async () => {
+        const user = {
+            email: 'novo_teste@email.com',
+            password: 'Senha@123'
+        };
 
+        const response = await userService.update(userId, user.email, user.password, transaction);
+
+        expect(response.user.email).toBe(user.email);
+        expect(response.user.password).toBe(user.password);
     });
 
     // Delete
-    it('remove a user', () => {
-
+    it('remove a user', async () => {
+        const response = await userService.destroy(userId, transaction);
+        expect(response).toBe(true);
     });
 });
